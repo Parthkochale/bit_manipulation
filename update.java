9424be2c2e46235cd303a019382bfa556ee4d1d1
Parth Kochale <piyushkochale5@gmail.com>
2026-09-24 18:59:11 +0530

    Add files via upload

---
+++ update.java
@@ -0,0 +1,38 @@
+public class update {
+  public static int getIthBit(int n , int i){
+    int bitMask = 1 << i;
+    if((n & bitMask) == 0){
+      return 0;
+    }else{
+      return 1;
+
+    }
+      
+
+  }
+
+
+  public static int setIthBit(int n , int i){
+    int bitMask = 1 << i;
+    return n | bitMask;
+  }
+
+
+  public static int clearIthBit(int n , int i){
+    int bitMask = ~(1 << i);
+    return n & bitMask;
+  }
+
+
+  public static int updateIthbit(int n, int i , int newBit){
+    if(newBit==0){
+      return clearIthBit(n,i);
+    } else {
+      return setIthBit(n,i);
+    }
+  }
+
+  public static void main (String args[]){
+    System.out.println(updateIthbit(10,2,1));
+  }
+}
